20aed06e782c3625554484e528bfe9274b6c6cf7
rafawessling <rafaoening@gmail.com>
2024-02-28 23:19:35 +0100

    style: Added font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="use-credentials" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+        <body className={inter.className}>
           <DataProvider>
             {children}
           </DataProvider>
