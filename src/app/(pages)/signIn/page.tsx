c2180b4a8040cc571c07cb9b36ccfff4e39af2b9
Diogo Evangelista Caxiado <caxiadodev@gmail.com>
2024-03-01 20:30:54 -0300

    refactor: Restruturando o login

--- src/app/(pages)/signIn/page.tsx
+++ src/app/(pages)/signIn/page.tsx
@@ -1,23 +1,24 @@
 'use client'
 
-import facebook from '@/app/assets/facebook.svg';
-import instagram from '@/app/assets/instagram.svg';
-import whatsapp from '@/app/assets/whatsapp.svg';
+import logo from '@/app/assets/logoLectio.svg';
 
 import Button from "@/app/components/Button/Button";
-import Input from "@/app/components/input/input";
+import Input from "@/app/components/Input/Input";
 
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { schemaSignIn, signinFormProps } from "@/app/schemas/schemaSignIn";
+import { useState } from 'react';
 
 import './signin.css';
 
 export default function SignIn() {
+    const [notFilledInputs, setNoFilledInputs] = useState(false);
+
     const { handleSubmit, register, formState:{ errors } } = useForm<signinFormProps>({
-        mode: 'onSubmit',
+        mode: 'onChange',
         resolver: zodResolver(schemaSignIn)
-    });
+    });    
 
     const handleData:SubmitHandler<signinFormProps> = (data) => {
         console.log('submit', data);
@@ -25,44 +26,41 @@ export default function SignIn() {
     }
 
     return (
-        <main>
-            <div className="signin-page">
+        <main className="signin-page">
+                <img src={logo} alt="logo da plataforma Lectio" />
+
                 <h2>Faça seu Login</h2>
-                <p>Embarque na aventura de conhecer o melhor da literatura brasileira</p>
+                <p className='signin-p'>Embarque na aventura de conhecer o melhor da literatura brasileira</p>
 
                 <form className="signin-form" onSubmit={handleSubmit(handleData)}>
                     <div>
                         <Input 
                             register={register('email', {required: 'campo obrigatório'})}  
                             errorMessage={errors.email && errors.email.message}
-                            label="E-mail" placeholder="Digite seu e-mail" type="email" 
+                            label="E-mail" placeholder="Digite seu endereço de e-mail" type="email" 
+                            security={false}
                         />
 
                         <Input 
                             register={register('password', {required: 'campo obrigatório'})}  
                             errorMessage={errors.password && errors.password.message} 
                             label="Senha" placeholder="Digite sua senha" type="password"
+                            security={true}
                         />
                     </div>
 
-                    <span>Esqueceu a senha?</span>
-                    <Button className="primary" title="Entrar" type="submit" />
+                    <span>Esqueceu sua senha?</span>
+                    
+                    <div className='signin-button'>
+                        <Button className="primary" title="Entrar" type="submit" size='full' disabled={notFilledInputs} />
+                    </div>
                 </form>
 
-                <span className="signin-redirect">Não tem login? <a href="./signUp">Cadastre-se</a></span>
-                
-                <div id="banner" />
-
+                <span className="signin-redirect">Não tem uma conta? <a href="./signUp">Cadastrar-se</a></span>
+            
                 <section className="signin-footer">
                     <span>Copyright 2022. Todos os direitos reservados</span>
-
-                    <article>
-                        <img src={facebook} alt="logo do facebook" />
-                        <img src={instagram} alt="logo do instagram" />
-                        <img src={whatsapp} alt="logo do whatsapp" />
-                    </article>
                 </section>
-            </div>
         </main>
     )
 }
