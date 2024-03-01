1c0c71537bfa045063b3a02ab1ad8f9ab11ff337
Diogo Evangelista Caxiado <caxiadodev@gmail.com>
2024-03-01 20:29:45 -0300

    feat: Props no Input

--- src/app/components/input/input.tsx
+++ src/app/components/input/input.tsx
@@ -1,27 +1,47 @@
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+
 import { Input } from '@/types/forms-type';
 import { UseFormRegisterReturn } from 'react-hook-form';
+import { useState } from 'react';
 
-import './input.css'
+import './Input.css';
 
 interface propsRegister {
     register?: UseFormRegisterReturn<string>,
     label?: string,
     placeholder?: string,
     value ?: string,
+    security ?: boolean,
     type?: string
     errorMessage?: string
 }
 
-export default function Input({label,placeholder,register,value, type, errorMessage}:propsRegister){
+export default function Input({label,placeholder,register,value, security, type, errorMessage}:propsRegister){    
+     const [viewInput, setViewInput] = useState(false);
+
+    function handleViewChange() {
+        setViewInput(!viewInput);
+    }
+
     return(
         <div className='input'>
             <label> {label} </label>
-            <input
-            placeholder={placeholder}
-            value={value}
-            type={type}
-            {...register}
-            />
+            <div className='security-password'>
+                <input  
+                placeholder={placeholder}
+                value={value}
+                type={security ? (viewInput ? 'text' : 'password') : type}
+                {...register}
+                />
+
+                {security && (
+                    viewInput ? 
+                        <FiEye size={'1.5rem'} onClick={handleViewChange} title='Botão para esconder a senha' /> 
+                        : 
+                        <FiEyeOff size={'1.5rem'} onClick={handleViewChange} title='Botão para visualizar a senha' />
+                )}
+
+            </div>
             <p>{errorMessage}</p>
 
         </div>
