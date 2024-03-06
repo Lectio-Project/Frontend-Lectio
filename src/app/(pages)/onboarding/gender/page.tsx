1f95e698ffada26d5a666648a58a03570bc15699
Diogo Caxiado <caxiadodev@gmail.com>
2024-03-06 19:35:46 -0300

    fix: Criação do Onboarding(gênero) responsivo

---
+++ src/app/(pages)/onboarding/gender/page.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import LogoWithName from '@/app/assets/logoWithName.svg';
+
+import ButtonGender from '@/app/components/ButtonGender/ButtonGender';
+import Button from '@/app/components/Button/Button';
+import { listGender } from '@/app/data/gender';
+
+import './gender.css';
+
+export default function Gender() {
+    function handleChangeClassName(e: React.MouseEvent<HTMLButtonElement>) {
+        if (e.currentTarget.classList.value === 'button-default-gender') {
+            e.currentTarget.classList.replace('button-default-gender', 'button-selected-gender');
+        } else {
+            e.currentTarget.classList.replace('button-selected-gender', 'button-default-gender');
+        }
+    }
+
+    return (
+        <main className='gender-container'>
+            <header><img src={LogoWithName} alt="Logo Lectio and name" className='gender-logo-with-name'/></header>
+
+            <div className='onboarding-step'>
+                <span>1/3</span>
+            </div>
+
+            <h2 className='gender-title'>Escolha 3 gêneros que você gosta</h2>
+
+            <section className='gender-buttons'>
+                {listGender.map((nameGender, key) => {
+                    return (
+                        <ButtonGender 
+                            key={key} 
+                            title={nameGender} 
+                            className="button-default-gender" 
+                            onClick={handleChangeClassName} 
+                        />
+                    );
+                })}
+            </section>
+
+            <div className='gender-button-action'>
+                <Button className='secondary' title='Pular' size='default' type='button'/>
+                <Button className='primary' title='Continuar' size='default' type='submit'/>
+            </div>
+        </main>
+    )
+}
