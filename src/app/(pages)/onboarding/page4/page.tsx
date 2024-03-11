4ba47aa0133c8eabf152d67902fa7cf17d939eea
rafawessling <rafaoening@gmail.com>
2024-03-11 22:40:47 +0100

    Added general style for Onboarding pages

--- src/app/(pages)/onboarding/page4/page.tsx
+++ src/app/(pages)/onboarding/page4/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import Button from '@/app/components/Button/Button';
 import BooksOnboarding from '@/app/components/BooksOnboarding/BooksOnboarding';
 import HeaderOnboarding from '@/app/components/HeaderOnboarding/HeaderOnboarding';
-
-import './page4.css';
 import FooterOnboarding from '@/app/components/FooterOnboarding/FooterOnboarding';
+
+import '@/app/styles/OnboardingSteps.css';
 
 export default function Page4() {
     return (
