0c53d29db131235ea4e05ee6559f263b8ea7ab38
rafawessling <rafaoening@gmail.com>
2024-03-12 21:29:28 +0100

    Added httpOnly to the setCookie function

--- src/utils/cookies.tsx
+++ src/utils/cookies.tsx
@@ -4,7 +4,10 @@ import { cookies } from 'next/headers'
 
 export async function setCookie(name: string, value: string) {
     const expirationTime = Date.now() + (8 * 60 * 60 * 1000);
-    cookies().set(name, value, { expires: expirationTime });
+    cookies().set(name, value, { 
+        expires: expirationTime,
+        httpOnly: true
+    });
 }
 
 export async function getCookie(name: string) {
