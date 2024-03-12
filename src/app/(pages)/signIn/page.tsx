1f42a7b59006a47451628e201262d3f5e8d67bb2
rafawessling <rafaoening@gmail.com>
2024-03-12 21:42:01 +0100

    Removed import from context

--- src/app/(pages)/signIn/page.tsx
+++ src/app/(pages)/signIn/page.tsx
@@ -14,14 +14,12 @@ import { schemaSignIn, signinFormProps } from "@/app/schemas/schemaSignIn";
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useDataContext } from '@/context/user';
+import { setCookie } from '@/utils/cookies';
 
 import './signin.css';
-import { setCookie } from '@/utils/cookies';
 
 export default function SignIn() {
     const [responseError, setResponseError] = useState({});
-    const { setUserData } = useDataContext();
     const router = useRouter();
 
     const { handleSubmit, register, formState:{ errors }} = useForm<signinFormProps>({
@@ -36,7 +34,7 @@ export default function SignIn() {
             const response = await api.post('/users/sign-in', {email, password});
             
             if (response.status === 201) {
-                setCookie('token', response.data.token);
+                await setCookie('token', response.data.token);
                 setResponseError({});
                 return router.replace('/onboarding/page1')
             }
