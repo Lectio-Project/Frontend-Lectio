bd00ffa6f3a2f21562b15cfcb67c6ce9ef649597
rafawessling <rafaoening@gmail.com>
2024-04-06 00:57:43 +0200

    Added search icon to Header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -3,25 +3,27 @@
 import Logo from '../../assets/logoWithName.svg';
 import Search from '../../assets/search.svg'
 import MenuIcon from '../../assets/menuIcon.svg';
-
 import NavBar from '@/app/components/NavBar/navBar';
+
 import { useDataContext } from '@/context/user';
+import { useRouter } from "next/navigation";
 
 import './Header.css';
 import HamburguerMenu from '../hamburguerMenu/hamburguerMenu';
 
 interface HeaderProps {
     search: 'able' | 'disabled';
-    select: 'home' | 'feed' | 'perfil';
+    select: 'home' | 'feed' | 'perfil' | 'none';
 }
 
 export default function Header({ search, select }: HeaderProps) {
     const { setOpenDrawer } = useDataContext();
+    const router = useRouter();
 
     return (
         <header className='header-container'>
             <div className='logo-section'>
-            <img src={Logo} alt='logo Icon'/>
+                <img src={Logo} alt='logo Icon' onClick={() => router.push('/home')}/>
             </div>
 
             <div className={search === 'able' ? 'header-search' : 'header-search-disabled'}>
@@ -29,8 +31,9 @@ export default function Header({ search, select }: HeaderProps) {
                 <input type='search' placeholder='Pesquisar'></input>
             </div>
 
-            <div className='sandwich-menu' onClick={()=> setOpenDrawer(true)}>
-                <img src={MenuIcon} alt='sandwich menu' />
+            <div className='sandwich-menu'>
+                <img src={Search} alt="search image" className='search-button-header' onClick={() => router.push('/search')}/>
+                <img src={MenuIcon} alt='sandwich menu' onClick={()=> setOpenDrawer(true)}/>
             </div>
 
             <nav>
