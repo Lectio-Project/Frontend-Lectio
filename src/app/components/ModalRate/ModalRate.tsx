d0bd813253dfa711ace3828ecc3a9d987296fc0f
staviasz <erickesc21@hotmail.com>
2024-04-05 17:03:38 -0300

    correção de tipagem do ratestars

--- src/app/components/ModalRate/ModalRate.tsx
+++ src/app/components/ModalRate/ModalRate.tsx
@@ -2,16 +2,16 @@ import CloseIcon from '../../assets/closeIcon.svg';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 
-import RatingStars from '@/app/components/RatingStars/RatingStars';
 import ButtonLectio from '@/app/components/Button/Button';
+import RatingStars from '@/app/components/RatingStars/RatingStars';
 
-import { useState } from 'react';
+import api from '@/api/api';
 import { useDataContext } from '@/context/user';
 import { getCookie } from '@/utils/cookies';
-import api from '@/api/api';
+import { useState } from 'react';
 
 import './ModalRate.css';
 
@@ -22,67 +22,92 @@ interface ModalRateProps {
     setAddComment: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export default function ModalRate({title, bookId, addComment, setAddComment}: ModalRateProps) {
+export default function ModalRate({
+    title,
+    bookId,
+    addComment,
+    setAddComment
+}: ModalRateProps) {
     const [showModal, setShowModal] = useState<boolean>(false);
-    const [text, setText] = useState<string>("");
-    const {rateValue, setRateValue} = useDataContext();
-  
+    const [text, setText] = useState<string>('');
+    const { rateValue, setRateValue } = useDataContext();
+
     function handleOpen() {
-    setShowModal(true);
+        setShowModal(true);
     }
 
     function handleClose() {
-    setShowModal(false);
+        setShowModal(false);
     }
 
     async function handleRate() {
         try {
             const token = await getCookie('token');
-            const request = {text: text, bookGrade: rateValue, bookId: bookId}
+            const request = {
+                text: text,
+                bookGrade: rateValue,
+                bookId: bookId
+            };
 
             await api.post(`/comments`, request, {
                 headers: {
-                authorization: `Bearer ${token}`
-                },
+                    authorization: `Bearer ${token}`
+                }
             });
 
             setAddComment(addComment + 1);
             handleClose();
         } catch (error: any) {
-            console.error(error)
-        } 
+            console.error(error);
+        }
     }
-    
+
     return (
-        <div className='container-modalRate'>
-            <Button onClick={handleOpen} className='button-rate'>
-                <RatingStars starsValues={0} returnValue size='medium' readOnly />
+        <div className="container-modalRate">
+            <Button onClick={handleOpen} className="button-rate">
+                <RatingStars
+                    starsValues={0}
+                    returnValue
+                    size="medium"
+                    readOnly
+                />
                 <span>{title}</span>
             </Button>
-            <Modal
-                open={showModal}
-                onClose={handleClose}
-                disableRestoreFocus
-            >
-                <Box className='modal-rate'>
-                    <img className='button-close' src={CloseIcon} alt='close icon' onClick={handleClose} /> 
+            <Modal open={showModal} onClose={handleClose} disableRestoreFocus>
+                <Box className="modal-rate">
+                    <img
+                        className="button-close"
+                        src={CloseIcon}
+                        alt="close icon"
+                        onClick={handleClose}
+                    />
 
-                    <Typography id='modal-title' variant="h3">
+                    <Typography id="modal-title" variant="h3">
                         {title}
                     </Typography>
 
-                    <div className='modal-rating'>
-                        <RatingStars size='large' returnValue/>
+                    <div className="modal-rating">
+                        <RatingStars size="large" returnValue />
                     </div>
-                    
-                    <div className='modal-comment'>
-                        <Typography id="modal-comment-title" variant='caption'>
+
+                    <div className="modal-comment">
+                        <Typography id="modal-comment-title" variant="caption">
                             Comentário
                         </Typography>
 
-                        <textarea className='modal-comment-area' placeholder='Escreva aqui o que achou da obra' onChange={(e) => setText(e.target.value)} />
+                        <textarea
+                            className="modal-comment-area"
+                            placeholder="Escreva aqui o que achou da obra"
+                            onChange={(e) => setText(e.target.value)}
+                        />
 
-                        <ButtonLectio className='modal-comment-button' title='Avaliar' type='submit' disabled={rateValue === 0 ? true : false} onClick={handleRate}/>
+                        <ButtonLectio
+                            className="modal-comment-button"
+                            title="Avaliar"
+                            type="submit"
+                            disabled={rateValue === 0 ? true : false}
+                            onClick={handleRate}
+                        />
                     </div>
                 </Box>
             </Modal>
--- src/app/components/RatingStars/RatingStars.tsx
+++ src/app/components/RatingStars/RatingStars.tsx
@@ -1,9 +1,9 @@
-import { Rating } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
+import { Rating } from '@mui/material';
 
-import './RatingStars.css'
-import { Star } from '@mui/icons-material';
 import { useDataContext } from '@/context/user';
+import { Star } from '@mui/icons-material';
+import './RatingStars.css';
 
 interface PropRatingStars {
     starsValues?: number;
@@ -14,34 +14,68 @@ interface PropRatingStars {
     returnValue?: boolean;
 }
 
-export default function RatingStars({starsValues, size, readOnly, bookValue, authorValue, returnValue}: PropRatingStars) {
-    const {setRateValue} = useDataContext();
-
-    const handleRatingChange = (event: any, newValue: number) => {
-        setRateValue(newValue);
-    };
+export default function RatingStars({
+    starsValues,
+    size,
+    readOnly,
+    bookValue,
+    authorValue,
+    returnValue
+}: PropRatingStars) {
+    const { setRateValue } = useDataContext();
 
     const customIcons = {
         star: {
-            icon: <Star style={{ fontSize: size === 'small' ? '16px' : (size === 'medium' ? '24px' : '34px') }}/>   
+            icon: (
+                <Star
+                    style={{
+                        fontSize:
+                            size === 'small'
+                                ? '16px'
+                                : size === 'medium'
+                                ? '24px'
+                                : '34px'
+                    }}
+                />
+            )
         },
         empty: {
-            icon: <StarIcon className='starIcon' style={{ fontSize: size === 'small' ? '16px' : (size === 'medium' ? '24px' : '34px') }} />
-        },
+            icon: (
+                <StarIcon
+                    className="starIcon"
+                    style={{
+                        fontSize:
+                            size === 'small'
+                                ? '16px'
+                                : size === 'medium'
+                                ? '24px'
+                                : '34px'
+                    }}
+                />
+            )
+        }
     };
     return (
-        <div className='ratingStars'>
-            <Rating 
-                defaultValue={starsValues || 0} 
-                precision={bookValue || authorValue ? 0.1 : 0.5} 
-                size={size} 
-                readOnly={readOnly} 
-                emptyIcon={customIcons.empty.icon} 
-                icon={customIcons.star.icon} 
-                onChange={returnValue && handleRatingChange} 
+        <div className="ratingStars">
+            <Rating
+                defaultValue={starsValues || 0}
+                precision={bookValue || authorValue ? 0.1 : 0.5}
+                size={size}
+                readOnly={readOnly}
+                emptyIcon={customIcons.empty.icon}
+                icon={customIcons.star.icon}
+                onChange={(e, value) => {
+                    returnValue && setRateValue(value!);
+                }}
             />
-            {bookValue && <strong className='rating-note-book'>{bookValue !== 0 ? bookValue.toFixed(1) : bookValue}</strong>}
-            {authorValue && <span className='rating-note-author'>({authorValue})</span>}
+            {bookValue && (
+                <strong className="rating-note-book">
+                    {bookValue !== 0 ? bookValue.toFixed(1) : bookValue}
+                </strong>
+            )}
+            {authorValue && (
+                <span className="rating-note-author">({authorValue})</span>
+            )}
         </div>
-    )
+    );
 }
