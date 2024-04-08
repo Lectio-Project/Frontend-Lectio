3427edbec377d325ef2249274f8cf64306780bcf
alissonromaosantos <eualissonromaosantos@gmail.com>
2024-04-07 23:13:32 -0300

    feat: feed incompleto

--- src/app/(pages)/(logged-routes)/feed/[category]/page.tsx
+++ src/app/(pages)/(logged-routes)/feed/[category]/page.tsx
@@ -1,13 +1,59 @@
 'use client'
 
+import api from "@/api/api";
 import BookHighlight from "@/app/components/BookHighlight/BookHighlight";
 import Header from "@/app/components/Header/Header";
-import { useDataContext } from "@/context/user"
+import { useDataContext } from "@/context/user";
+import { BookProps } from "@/types/book";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 import './category.css';
 
-export default function Category() {
-  const { booksSelected, titleSelected } = useDataContext();
-  console.log(booksSelected)
+type categoriesParams = {
+  params: { category: string }
+}
+export default function Category({ params }: categoriesParams) {
+  const { booksSelected, setBooksSelected, titleSelected } = useDataContext();
+  const session = useSession()
+  const paramsValue = params.category === 'sexGenderAuthor' ? 'women' : 'true'
+
+  async function getData() {
+    try {
+      const response = await api.get(`/search/categories?${params.category}=${paramsValue}`, {
+        headers: {
+          Authorization: `Bearer ${session.data?.token}`
+        }
+      })
+
+    if (params.category === 'literaryAwards') {
+      const { literaryAwards } = response.data;
+
+      const literaryAwardJabuti: BookProps[] = literaryAwards.filter((bookAward: BookProps) => {
+        const jabutiAward = bookAward.LiteraryAwards?.some((book) => {  
+          return book.name.includes('Jabuti') || book.name.includes('jabuti')
+        })
+
+        return jabutiAward && bookAward
+      })
+
+      setBooksSelected(literaryAwardJabuti)
+      return
+    }
+
+    setBooksSelected(response.data)
+   
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (!booksSelected.length) {
+      console.log(true)
+      getData();
+    }
+  }, [session])
+
   return (
     <>
       <Header select="feed" search="able" />
